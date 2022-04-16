abfcd5c9e4f633d864f610b83e9e6d9091abc307
Suyog Adhikari <suyogadhiakri@gmail.com>
2022-04-16 19:20:58 +0545

    Completed Feedback UI

--- src/component/FeedbackList.jsx
+++ src/component/FeedbackList.jsx
@@ -1,18 +1,19 @@
 import {motion, AnimatePresence} from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
+import Spinner from './shared/Spinner'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
+    const { feedback, isLoading } = useContext(FeedbackContext)
 
-    if(!feedback || feedback.length === 0) {
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback Yet</p>
     }
 
-        return(
-            <div className='feedback-list'>
-                <AnimatePresence>
+    return isLoading ? (<Spinner />) : (
+        <div className='feedback-list'>
+            <AnimatePresence>
                 {feedback.map((item) => (
                     <motion.div 
                         key={item.id}
@@ -23,9 +24,9 @@ function FeedbackList() {
                         <FeedbackItem key={item.id} item={item} />
                     </motion.div>
                 ))}
-                </AnimatePresence>
-            </div>
-        )
+            </AnimatePresence>
+        </div>
+    )
 }
 
 export default FeedbackList
---
+++ src/component/shared/Spinner.jsx
@@ -0,0 +1,16 @@
+import spinner from '../assets/spinner.gif'
+
+function Spinner() {
+  return (
+    <img 
+        src={spinner} 
+        alt="Loading..." 
+        style={{ width: '100px',
+                 margin: 'auto',
+                 display:'block'
+        }} 
+    />
+  )
+}
+
+export default Spinner
